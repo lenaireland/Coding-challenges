4565adf99590ae4fb7590314e5627d3816224c2d
Lena Ireland <lenaireland@gmail.com>
2018-11-30 00:46:26 +0000

    Finished binary-tree problem

--- binary-tree.py
+++ binary-tree.py
@@ -1,20 +1,43 @@
-def minimal_binary_tree(lst, current=None):
-    """Given list of integers, build binary tree of minimal height"""
+class Node():
 
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def minimal_binary_tree(lst):
+    """Given list of integers, build binary tree of minimal height"""
+        
     if lst == []:
-        return []
+        return None
+
+    if len(lst) == 1:
+        return Node(lst[0])
+
 
 
     current = lst[int(len(lst)/2)]
 
-    left = lst[:int(len(lst)/2)]
+    node = Node(current)
 
-    right = lst[int(len(lst)/2)+1:]
+    node.left = minimal_binary_tree(lst[:int(len(lst)/2)])
 
-    # print(current)
-    # print(left)
-    # print(right)
+    node.right = minimal_binary_tree(lst[int(len(lst)/2)+1:])
 
-    tree = minimal_binary_tree(left, current) + minimal_binary_tree(right, current)
+    # checking
+    print("Current is {}".format(current))
+    print("Node.data is {}".format(node.data))
+    try:
+        print("Node.left.data is {}".format(node.left.data))
+    except:
+        pass
+    try:
+        print("Node.right.data is {}".format(node.right.data))
+    except:
+        pass
+
+    return node
+
 
 
