ea49bf38291bf4a73f39c21b02485a933df08ba0
Lena Ireland <lenaireland@gmail.com>
2018-11-30 01:37:32 +0000

    Finished robot maze problem

--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-def maze_path(matrix, path=None):
+def maze_path(matrix, r=None, c=None):
     """Traverse T/F maze.  Robot can only move right or down.
     If robot can get to finish, return coords of route.
     Otherwise return None"""
@@ -6,35 +6,43 @@ def maze_path(matrix, path=None):
     n_rows = len(matrix)
     n_cols = len(matrix[0])
 
-    finish_r = n_rows - 1
-    finish_c = n_cols - 1
+    if r is None and c is None:
+        r = n_rows - 1
+        c = n_cols -1
+        return maze_path(matrix, r, c)
 
-    r = 0
-    c = 0
+    # print("Current point is ({},{})".format(r,c))
 
-    if matrix[0][0] == "F":
-        return None
+    if r < 0 or c < 0 or matrix[r][c] is False:
+        return False
+
+    if r == 0 and c == 0:
+        return [(r,c)]
+
+    up_path = maze_path(matrix, r-1, c)
+    left_path = maze_path(matrix, r, c-1)
+
+    if up_path:
+        return up_path + [(r,c)]
+    elif left_path:
+        return left_path + [(r,c)]
+
+    return None
 
 
-    if r == finish_r and c == finish_c:
-        return (r,c)
+invalid = ([[True, False, False, True, False, True],
+           [True, True, False, True, True, True],
+           [True, False, False, True, True, True],
+           [True, False, True, True, True, True],
+           [True, False, True, True, True, True]])
 
-    try:
-        if matrix[r+1][c] == "T":
-            value = maze_path(matrix[:][1:], path)
-            if value:
-                path.append((r,c))
-    except:
-        pass
+valid =  ([[True, False, False, True, True, True],
+           [True, True, True, False, True, True],
+           [True, False, True, False, True, True],
+           [True, False, True, True, False, True],
+           [False, False, False, True, True, True]])
 
-    try:
-        if matrix[r][c+1] == "T":
-            value = maze_path(matrix[1:][:], path)
-            if value:
-                path.append((r,c))
-    except:
-        pass
+print(maze_path(invalid))
 
-    return path
-
+print(maze_path(valid))
 
