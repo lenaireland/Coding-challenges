96a6a7060f9c233e11c626b1c74ebd6cf3d21ed7
Lena Ireland <lenaireland@gmail.com>
2019-01-23 21:53:45 +0000

    Practice challenges

--- reverse_in_parens.py
+++ reverse_in_parens.py
@@ -1,4 +1,8 @@
 def reverseInParentheses(inputString):
+    """Write a function that reverses characters in (possibly nested) 
+    parentheses in the input string.
+
+    Input strings will always be well-formed with matching ()s."""
 
     string_list = []
     
